2bcda21b6b696915332c74d6406d0db8b4315315
Heshan Yodagama <yodagamaheshan@icloud.com>
2020-12-06 21:55:47 +0530

    Initial Commit

---
+++ SetGame-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetGame-SwiftUI
+//
+//  Created by Heshan Yodagama on 2020-12-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SetGame-SwiftUI/SetGame_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGame_SwiftUIApp.swift
+//  SetGame-SwiftUI
+//
+//  Created by Heshan Yodagama on 2020-12-06.
+//
+
+import SwiftUI
+
+@main
+struct SetGame_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
